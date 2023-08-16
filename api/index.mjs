0927823e5fc13768dd933feeabb446120bc0ff1f
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-08-16 15:34:51 +0400

    fixing cors issue

--- api/index.mjs
+++ api/index.mjs
@@ -4,9 +4,31 @@ import fetch from 'node-fetch';
 const captchaSecret = process.env.REACT_APP_CAPTCHA_SECRET;
 const app = express();
 const port = 3000; // Choose a port for your server
-app.use(cors({ origin: 'https://edgy-media.vercel.app' })); // Enable CORS for all routes
+app.use(cors({ origin: 'https://edgy-media.vercel.app/', })); // Enable CORS for all routes
 app.use(json());
+// const allowCors = fn => async (req, res) => {
+//     res.setHeader('Access-Control-Allow-Credentials', true)
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     // another common pattern
+//     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//     )
+//     if (req.method === 'OPTIONS') {
+//         res.status(200).end()
+//         return
+//     }
+//     return await fn(req, res)
+// }
 
+// const handler = (req, res) => {
+//     const d = new Date()
+//     res.end(d.toString())
+// }
+
+// module.exports = allowCors(handler)
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*')
 //     res.header('Access-Control-Allow-Headers', '*')
@@ -64,26 +86,3 @@ app.listen(port, () => console.log(`Server is running on http://localhost:${port
 
 
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
-
-const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
