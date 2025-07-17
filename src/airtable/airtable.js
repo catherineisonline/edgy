04cea20ab69f5d912f9b9b4e9f789005fa088373
catherineisonline <ekaterine.jpg@gmail.com>
2025-07-17 18:46:58 +0700

    remove typo

--- src/airtable/airtable.js
+++ src/airtable/airtable.js
@@ -1,7 +1,7 @@
 import Airtable from "airtable";
 
 const apiKey = process.env.VITE_AIRTABLE_KEY;
-const baseId = process.meta.env.VITE_AIRTABLE_ID;
+const baseId = process.env.VITE_AIRTABLE_ID;
 console.log(apiKey, baseId);
 
 const edgyBase = new Airtable({ apiKey: apiKey }).base(baseId);
